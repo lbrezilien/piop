5f1daab37ab375b8b4c8ad3cf24c2f8ef4efdc4f
lbrezilien <lbrezilien@gmail.com>
2015-12-12 18:51:48 -0500

    adds crud and restful actions for sheets

--- api/controllers/SheetsController.js
+++ api/controllers/SheetsController.js
@@ -5,7 +5,56 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
-};
+ module.exports = {
 
+ 		index: function(req, res){
+ 				Sheets.find().exec(function (err, sheets){
+ 				    if (err) return res.serverError(err);
+ 						// res.json(sheets);
+ 				 		res.view('sheets/index',{
+ 								sheets: sheets
+ 						});
+ 				});
+ 		 },
+
+ 		new: function(req, res){
+ 				res.view();
+ 		 },
+
+ 		create: function(req, res){
+ 				Sheets.create(req.params.all(), function(err, sheet){
+ 								res.redirect('/sheets/show/'+sheet.id);
+ 				});
+ 			},
+
+ 		edit: function(req, res){
+ 					Sheets.findOne(req.param('id'), function(err, sheet){
+ 									res.view({
+ 										  sheet: sheet
+ 									})
+ 					});
+ 				},
+
+ 		update: function(req, res){
+ 				Sheets.update(req.param('id'), req.params.all(), function(err, sheet){
+
+ 								res.redirect('/sheets/show/'+req.param('id'));
+ 					});
+ 				},
+
+
+ 		show: function(req, res){
+ 					Sheets.findOne(req.param('id'), function(err, sheet){
+ 									res.view({
+ 										  sheet: sheet
+ 									})
+ 					});
+ 				},
+
+ 		destroy: function(req, res){
+ 						Sheets.findOne(req.param('id'), function(err, sheet){
+ 										sheet.destroy();
+ 										res.redirect('/sheets');
+ 						});
+ 					}
+ };
